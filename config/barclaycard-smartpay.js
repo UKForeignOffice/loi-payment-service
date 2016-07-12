11862f01a2f6f27b5b71bab66d027e7b64da0b56
rp825 <rp825@york.ac.uk>
2016-07-12 17:30:35 +0100

    Merge remote-tracking branch 'refs/remotes/origin/Pre-Production' into Development

--- config/barclaycard-smartpay.js
+++ config/barclaycard-smartpay.js
@@ -1,9 +1,9 @@
 /**
  * Created by skaifem on 10/12/2015.
  */
- 
+
 var dotenv = require("dotenv");
-var env = dotenv.config(); 
+var env = dotenv.config();
 var HMAC = JSON.parse(env.HMAC);
 var skinCode = JSON.parse(env.SKINCODE);
 var currencyCode = JSON.parse(env.CURRENCYCODE);
@@ -15,26 +15,28 @@ var smartPayHMACTestUrl = JSON.parse(env.SMARTPAYHMACTESTURL);
 var startNewApplicationUrl = JSON.parse(env.STARTNEWAPPLICATIONURL);
 var mongoURL = JSON.parse(env.MONGOURL);
 var cookieDomain = env.PAYMENTMETHODS;
-var paymentMethods = JSON.parse(env.PAYMENTMETHODS); 
+var paymentMethods = JSON.parse(env.PAYMENTMETHODS);
 var db = JSON.parse(env.DATABASE);
+var live_variables = JSON.parse(env.LIVEVARIABLES);
 
 var configs = {
-"HMAC" : HMAC.HMAC, 
-"skinCode": skinCode.skinCode,
-"currencyCode": currencyCode.currencyCode,
-"merchantAccount": merchantAccount.merchantAccount,
-"resultURL":resultURL.resultURL,
-"applicationServiceReturnUrl" : applicationServiceReturnUrl.applicationServiceReturnUrl,
-"smartPayUrl" : smartPayUrl.smartPayUrl,
-"smartPayHMACTestUrl" : smartPayHMACTestUrl.smartPayHMACTestUrl,
-"startNewApplicationUrl" : startNewApplicationUrl.startNewApplicationUrl,
-"mongoURL": mongoURL.mongoURL,
-"cookieDomain": cookieDomain.cookieDomain
+    "HMAC" : HMAC.HMAC,
+    "skinCode": skinCode.skinCode,
+    "currencyCode": currencyCode.currencyCode,
+    "merchantAccount": merchantAccount.merchantAccount,
+    "resultURL":resultURL.resultURL,
+    "applicationServiceReturnUrl" : applicationServiceReturnUrl.applicationServiceReturnUrl,
+    "smartPayUrl" : smartPayUrl.smartPayUrl,
+    "smartPayHMACTestUrl" : smartPayHMACTestUrl.smartPayHMACTestUrl,
+    "startNewApplicationUrl" : startNewApplicationUrl.startNewApplicationUrl,
+    "mongoURL": mongoURL.mongoURL,
+    "cookieDomain": cookieDomain.cookieDomain
+
 };
 
 var database = db.database;
 
-var config = {configs: configs, paymentMethods: paymentMethods, database: database};
+var config = {configs: configs, paymentMethods: paymentMethods, database: database, "live_variables":live_variables};
 
 //console.log(JSON.stringify(configs));
 
