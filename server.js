2f80a2f3c431e8e373a3241bbac5c1278357c3ce
Benjamin Dickey <43062503+benjamindi96k@users.noreply.github.com>
2021-08-19 11:22:08 +0100

    Revert "Fix TLS for local dev"
    This reverts commit 0726dc5b681ad2e40f18c699cceebfd36e039f3d.

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ var store = new RedisStore({
         port: configSmartPay.sessionSettings.port,
         prefix: configSmartPay.sessionSettings.prefix,
         pass: configSmartPay.sessionSettings.password,
-        tls: process.env.NODE_ENV === 'development' ? undefined : {}
-});
+        tls: {}
+    });
 app.set('view engine', 'ejs');
 app.use(function (req, res, next) {
     res.locals = {
