d75de8f3cdd921d2099a7daa47a271012f2b466c
Simon Moore <s.moore@kainos.com>
2023-04-04 11:02:10 +0100

    Fix broken tests

--- server.js
+++ server.js
@@ -60,28 +60,6 @@ redisClient.on("error", (error) => {
 
 const redisStore = new RedisStore({ client: redisClient });
 
-let redisClientConnected = false;
-
-redisClient.on("ready", () => {
-    redisClientConnected = true;
-});
-
-app.use((req, res, next) => {
-    if (redisClientConnected) {
-        next();
-    } else {
-        redisClient.connect((err) => {
-            if (err) {
-                console.error("Redis client error:", err);
-                next(err);
-            } else {
-                redisClientConnected = true;
-                next();
-            }
-        });
-    }
-});
-
 app.use(
     session({
         store: redisStore,
