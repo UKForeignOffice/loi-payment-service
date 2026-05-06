f629dd2694c5ab2b47382c4a224f7436f4def787
Alistair Laing <aliuk2012@users.noreply.github.com>
2026-05-06 13:01:36 +0100

    Remove disablePaymentButton.js
    Originally this functinality was added t ostop users from being able to double click.
    GOV.UK Design System nunjucks macros have a configuration option to prevent double clicking.
    Since we will be using the GOV.UK Design System components directly there is no need for this
    implemation.
    
    Signed-off-by: Alistair Laing <aliuk2012@users.noreply.github.com>

--- styles/govuk/javascripts/disablePaymentButton.js
+++
@@ -1,7 +0,0 @@
-function disablePaymentButton() {
-    var disableButton = $('#paymentButton');
-    disableButton.text("Please wait...").prop('disabled', true);
-    setTimeout(function() {
-        disableButton.prop('disabled', false).text("Continue to payment");
-    }, 2000);
-}
